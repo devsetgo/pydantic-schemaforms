a7d70fd6dd4dd99fb7390da83dddea2fcb873180
devsetgo <mikeryan56@gmail.com>
2026-06-28 18:41:52 +0000

    cleanup

--- untagged-74efc607d73062a649c6/js/demo-link.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  document.querySelectorAll("a[href*='pydantic-schemaforms.devsetgo.com']").forEach(function (a) {
-    a.setAttribute("target", "_blank");
-    a.setAttribute("rel", "noopener noreferrer");
-  });
-});
