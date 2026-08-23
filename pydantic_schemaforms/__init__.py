8f46e7e5f7c74067a5ac24510f0b6171353e45ed
devsetgo <mikeryan56@gmail.com>
2026-08-23 23:01:18 +0000

    Bump of Version 26.3.4

--- pydantic_schemaforms/__init__.py
+++ pydantic_schemaforms/__init__.py
@@ -148,7 +148,7 @@ from .ai_instructions import (
     suggested_instruction_filename,
 )
 
-__version__ = '26.3.3'
+__version__ = '26.3.4'
 __package_name__ = 'pydantic-schemaforms'
 __author__ = 'Pydantic Forms Team'
 __description__ = 'Modern form generation library for Python 3.14+'
